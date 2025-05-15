fe6eb694bcc6f650ae708d0ff265973fb26b577a
Christophorosweb <Christophoroschile@gmail.com>
2025-05-14 23:18:01 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // URL y clave pública del proyecto
 export const supabaseUrl = 'https://tyqrzixrkrlcklhjvtfu.supabase.co';
-export const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5cXJ6aXhya3JsY2tsaGp2dGZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU1MDg2NzgsImV4cCI6MjAzMTA4NDY3OH0.hOkcBPYMZflGuCJrNhCIbAo2_C6Y23hU8JKwZkCb-xg';
+export const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5cXJ6aXhya3JsY2tsaGp2dGZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMzg2MDYsImV4cCI6MjA2MjgxNDYwNn0.TxhpbioYVXLKH5z4VQfIPzYH7it5c9Qaf-pyI-vWrLo';
 
 // Cliente de Supabase ya inicializado
 export const supabase = createClient(supabaseUrl, supabaseKey);
